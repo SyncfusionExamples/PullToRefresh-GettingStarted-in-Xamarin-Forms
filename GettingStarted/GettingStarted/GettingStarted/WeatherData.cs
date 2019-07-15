1dc322f40718b29914503b754944385461637f78
Deepika <deepika.balasubramaniyan@syncfusion.com>
2019-07-15 15:19:38 +0530

    GettingStarted page has updated.

---
+++ GettingStarted/GettingStarted/GettingStarted/WeatherData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GettingStarted
+{
+    public class WeatherData : INotifyPropertyChanged
+    {
+        private string _temperature;
+
+        public WeatherData(string temperature)
+        {
+            Temperature = temperature;
+        }
+
+        public string Temperature
+        {
+            get
+            {
+                return _temperature;
+            }
+            set
+            {
+                _temperature = value;
+                RaisePropertyChanged("Temperature");
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+
+        #endregion
+    }
+}
